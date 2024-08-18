2302b7fb1dedf7dcac7a941f6a98c77d2cdc31db
Rehan <muhammadrehan3024@gmail.com>
2024-08-18 09:58:27 +0500

    Refactored the "Suppliers", "Banks" and "All" screens.
    The header gradient widget was made.

---
+++ lib/widgets/main_screen_banks/gradient_header_bank.dart
@@ -0,0 +1,185 @@
+import 'package:digi_khata/screens/main_app_screens/home/all.dart';
+import 'package:digi_khata/screens/main_app_screens/home/customers.dart';
+import 'package:digi_khata/screens/main_app_screens/home/suppliers.dart';
+import 'package:flutter/material.dart';
+
+class HeaderGradientBank extends StatelessWidget {
+  const HeaderGradientBank({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: 130,
+      padding: EdgeInsets.only(left: 20, right: 20, top: 2, bottom: 20),
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(
+          colors: [
+            Color.fromARGB(255, 218, 98, 0),
+            Color.fromARGB(255, 255, 30, 0),
+          ],
+          begin: Alignment.centerLeft,
+          end: Alignment.centerRight,
+        ),
+      ),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Row(
+                children: [
+                  Text(
+                    'atomcamp',
+                    style: TextStyle(
+                      fontSize: 18,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white,
+                    ),
+                  ),
+                  SizedBox(width: 2),
+                  Icon(
+                    Icons.keyboard_arrow_down_sharp,
+                    color: Colors.white,
+                    size: 24,
+                  ),
+                ],
+              ),
+              OutlinedButton.icon(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.notifications_none_sharp,
+                  color: Colors.blue,
+                ),
+                label: const Text(
+                  'BULK REMINDER',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 95, 183, 255),
+                    fontWeight: FontWeight.normal,
+                    fontSize: 13,
+                  ),
+                ),
+                style: OutlinedButton.styleFrom(
+                  side: const BorderSide(
+                      color: Color.fromARGB(255, 60, 167, 255), width: 2),
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 7, vertical: 5),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(40),
+                  ),
+                  backgroundColor: Colors.white,
+                ),
+              ),
+            ],
+          ),
+          Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: [
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          Customers(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(-1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOut;
+
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                            position: offsetAnimation, child: child);
+                      },
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Customers',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 216, 216, 216),
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          Suppliers(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(-1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOut;
+
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                            position: offsetAnimation, child: child);
+                      },
+                    ),
+                  );
+                },
+                child: const Text(
+                  'Suppliers',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 216, 216, 216),
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {},
+                child: const Text(
+                  'Banks',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 216, 216, 216),
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+              TextButton(
+                onPressed: () {
+                  Navigator.of(context).push(
+                    PageRouteBuilder(
+                      pageBuilder: (context, animation, secondaryAnimation) =>
+                          All(),
+                      transitionsBuilder:
+                          (context, animation, secondaryAnimation, child) {
+                        const begin = Offset(1.0, 0.0);
+                        const end = Offset.zero;
+                        const curve = Curves.easeInOut;
+
+                        var tween = Tween(begin: begin, end: end)
+                            .chain(CurveTween(curve: curve));
+                        var offsetAnimation = animation.drive(tween);
+
+                        return SlideTransition(
+                            position: offsetAnimation, child: child);
+                      },
+                    ),
+                  );
+                },
+                child: const Text(
+                  'All',
+                  style: TextStyle(
+                    color: Color.fromARGB(255, 216, 216, 216),
+                    fontSize: 14,
+                  ),
+                ),
+              ),
+            ],
+          ),
+        ],
+      ),
+    );
+  }
+}
