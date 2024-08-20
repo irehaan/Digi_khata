4cb282f5630d03d0f0fcbc42c4d60596fae6076b
Rehan <muhammadrehan3024@gmail.com>
2024-08-20 19:19:22 +0500

    Configured Firebase

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,22 @@
+import 'package:digi_khata/firebase_options.dart';
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'screens/splash.dart';
 
-void main() {
-  runApp(MyApp());
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp(
+    options: DefaultFirebaseOptions.currentPlatform,
+  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
+    return const MaterialApp(
       home: SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
